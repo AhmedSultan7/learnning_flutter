1eb824b9a005e9a5acbc52cbe772ddd8a46e028a
200017699@must.edu.eg <200017699@must.edu.eg>
2024-08-25 21:55:36 +0300

    first stage of learninng flutter.

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+
+import 'modules/login_screen/login_screen.dart';
+
+void main()
+{
+  runApp(MyApp());
+}
+
+// Stateless
+// Stateful
+
+// class MyApp
+
+class MyApp extends StatelessWidget
+{
+  // constructor
+  // build
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
+}
